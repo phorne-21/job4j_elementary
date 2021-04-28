8790acf04495e66eb7a35ff02976ed7f6e90e3d7
phorne-21 <lwrow@rambler.ru>
2021-04-28 13:41:12 +0300

    3.3. Разбор первой программы. [no number]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ekaterina Filippova");
+        System.out.println("25.08.1990");
+
+    }
+}
