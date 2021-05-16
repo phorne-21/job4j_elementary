2f0ab811d0bbf30ca620b9c19a310320035ab051
phorne-21 <lwrow@rambler.ru>
2021-05-16 20:50:10 +0300

    5.7. Ипотека [#156315 #1468]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        for (int i = 0; amount > 0; i++) {
+        while (amount > 0) {
             amount = amount + amount * percent / 100 - salary;
             year++;
         }
